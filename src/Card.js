4db40806303a12ae493e4773f72732924ee3ab36
shubhampant13 <shubhampant13@gmail.com>
2018-08-06 12:03:44 +0530

    Component Creation (Cards) along with Destructuring, use of React , and Robohash.org

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = (props) => {
+		return(
+            <div className="tc bg-light-green ma2 pa3 dib br3 grow">
+     			<img alt="Images" src={`https://robohash.org/${props.id}?set=set2&size=190x200`} />
+     			<div>
+                     <h2>{props.name}</h2>
+                     <p>{props.email}</p>  
+     			</div>
+            </div>
+			);
+
+}
+
+export default Card;
